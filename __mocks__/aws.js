240b5d0ab88d6b093d449bfa7a5c9dda235d9df8
mshap <shapirom@gmail.com>
2021-05-02 20:45:09 -0400

    implement s3 mocks

--- __mocks__/aws.js
+++
@@ -1,9 +0,0 @@
-console.log("mocked!")
-
-// module.exports = {
-//     save: storeProduct,
-//     get: getFile,
-//     getProduct: getProduct,
-//     list: listLatest,
-//     send: send
-// }
--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,7 +1,20 @@
-jest.mock('aws')
-jest.mock('aws-sdk')
 
 const utils = require('../src/api/utils')
+const AWS = require('aws-sdk')
+
+jest.mock('aws-sdk', () => {
+    const S3Mocked = {
+        listObjects: jest.fn().mockReturnThis(),
+        getObject: jest.fn().mockReturnThis(),
+        promise: jest.fn()
+    }
+
+    return {
+        S3: jest.fn(() => S3Mocked)
+    }
+})
+
+const s3 = new AWS.S3();
 
 test('Has New Data', () => {
     expect(utils.isChanged({
@@ -22,4 +35,68 @@ test('Not new data', () => {
         now: 4,
         prior: 2
     })).toBeFalsy()
+})
+
+describe('Who knows', () => {
+    beforeEach(() => {
+        (s3.listObjects().promise).mockReset()
+        s3.getObject().promise.mockReset()
+    })
+
+    it('Nothing Returned', async () => {
+        expect(jest.isMockFunction(s3.listObjects)).toBeTruthy()
+        expect(jest.isMockFunction(s3.listObjects().promise)).toBeTruthy()
+        s3.listObjects().promise.mockResolvedValueOnce({
+            Contents: []
+        })
+
+        const res = await utils.compareData()
+
+        expect(res).toEqual({now:0, prior:0})
+    })
+
+    it('One Object', async () => {
+        expect(jest.isMockFunction(s3.getObject)).toBeTruthy()
+        expect(jest.isMockFunction(s3.getObject().promise)).toBeTruthy()
+        s3.listObjects().promise.mockResolvedValueOnce({
+            Contents: [{LastModified: "April 17, 2021, 06:30:20"}]
+        })
+        s3.getObject().promise.mockResolvedValueOnce({Body: Buffer.from(JSON.stringify({totalQuantity:3}), "utf-8")})
+
+        const res = await utils.compareData()
+
+        expect(res).toEqual({now:3, prior:0})
+    })
+
+    it('Two Objects', async () => {
+        s3.listObjects().promise.mockResolvedValueOnce({
+            Contents: [
+                {LastModified: "April 17, 2021, 06:30:20"},
+                {LastModified: "April 18, 2021, 06:30:20"}
+            ]
+        })
+        s3.getObject().promise.mockResolvedValueOnce({Body: Buffer.from(JSON.stringify({totalQuantity:3}), "utf-8")})
+        s3.getObject().promise.mockResolvedValueOnce({Body: Buffer.from(JSON.stringify({totalQuantity:2}), "utf-8")})
+
+        const res = await utils.compareData()
+
+        expect(res).toEqual({now:3, prior:2})
+    })
+
+    it('More Than Two Objects', async () => {
+        s3.listObjects().promise.mockResolvedValueOnce({
+            Contents: [
+                {LastModified: "April 17, 2021, 06:30:20"},
+                {LastModified: "April 18, 2021, 06:30:20"},
+                {LastModified: "April 19, 2021, 06:30:20"},
+            ]
+        })
+        s3.getObject().promise.mockResolvedValueOnce({Body: Buffer.from(JSON.stringify({totalQuantity:3}), "utf-8")})
+        s3.getObject().promise.mockResolvedValueOnce({Body: Buffer.from(JSON.stringify({totalQuantity:2}), "utf-8")})
+        s3.getObject().promise.mockResolvedValueOnce({Body: Buffer.from(JSON.stringify({totalQuantity:5}), "utf-8")})
+
+        const res = await utils.compareData()
+
+        expect(res).toEqual({now:3, prior:2})
+    })
 })
