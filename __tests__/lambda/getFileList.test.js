934c7ef59b88c0afe1f506bfa07a1565db268295
mshap <shapirom@gmail.com>
2021-05-16 08:36:03 -0400

    dynamic tests for file read

--- __tests__/lambda/getFileList.test.js
+++ __tests__/lambda/getFileList.test.js
@@ -1,4 +1,4 @@
-const lambda = require('../../src/lambda/getProducts')
+const lambda = require('../../src/lambda/getFileList')
 const cloud = require('../../src/api/aws')
 
 jest.mock('../../src/api/aws', () => {
@@ -7,32 +7,43 @@ jest.mock('../../src/api/aws', () => {
     }
 })
 
+const mocks = {
+    'empty': [],
+    'load': ['123']
+}
+
 describe('Load Products File', () => {
     afterEach(() => {
         cloud.get.mockReset()
     })
 
     it('load empty file', async() => {
-        cloud.get.mockResolvedValueOnce([])
+        const input = 'empty'
+        cloud.get.mockResolvedValueOnce(mocks[input])
         expect(jest.isMockFunction(cloud.get)).toBeTruthy()
-        const res = await lambda.handler({bucket: 'Test'})
+        const res = await lambda.handler({bucket: 'Test', inputFile: input})
 
         expect(res).toEqual({
             bucket: 'Test',
+            inputFile: input,
             workList: ['SENTINEL'],
             next: 'SENTINEL'
         })
+        expect(cloud.get).toBeCalledWith("Test", input)
     })
 
     it('load valid file file', async() => {
-        cloud.get.mockResolvedValueOnce(['123'])
+        const input = 'load'
+        cloud.get.mockResolvedValueOnce(mocks[input])
         expect(jest.isMockFunction(cloud.get)).toBeTruthy()
-        const res = await lambda.handler({bucket: 'Test'})
+        const res = await lambda.handler({bucket: 'Test', inputFile: input})
 
         expect(res).toEqual({
             bucket: 'Test',
+            inputFile: input,
             workList: ['123','SENTINEL'],
             next: '123'
         })
+        expect(cloud.get).toBeCalledWith("Test", input)
     })
 })
